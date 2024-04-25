763f9729ce8ab5981a5e839e3806adacb191e036
LemonKnife <linwentao99@gmail.com>
2024-04-25 12:15:41 +0800

    feat(nvim): modify lsp loading icons
    use FiraCode specified icons to show lsp loading process

--- dot_config/nvim/lua/plugins/ui/statusbar/init.lua
+++ dot_config/nvim/lua/plugins/ui/statusbar/init.lua
@@ -1,4 +1,18 @@
 return {
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  {
+    "AstroNvim/astroui",
+    opts = {
+      -- NOTE: Use FiraCode Nerd Font to show LSPLoadng icons
+      icons = {
+        LSPLoading1 = "",
+        LSPLoading2 = "",
+        LSPLoading3 = "",
+        LSPLoading4 = "",
+        LSPLoading5 = "",
+        LSPLoading6 = "",
+      },
+    },
+  },
 }
